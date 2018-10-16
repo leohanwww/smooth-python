03921c22185ea8c94449e7d369818647f779d488
leohanwww <pumpkinlittlehan@hotmail.com>
2018-10-16 21:01:02 +0800

    晚上在家继续

--- 4 文本和字节序列.py
+++ 4 文本和字节序列.py
@@ -61,4 +61,72 @@ UnicodeDecodeError: 'utf-8' codec can't decode byte 0xe9 in position 5: invalid 
 'Montr�al' #无法显示的字符被替代了
 
 处理文本文件
+>>> fp = open('cafe.txt', 'w', encoding='utf_8')
+>>> fp ➊
+<_io.TextIOWrapper name='cafe.txt' mode='w' encoding='utf_8'>
+>>> fp.write('café')
+4 ➋
+>>> fp.close()
+>>> import os
+>>> os.stat('cafe.txt').st_size
+5 ➌
+>>> fp2 = open('cafe.txt')
+>>> fp2 ➍
+<_io.TextIOWrapper name='cafe.txt' mode='r' encoding='cp1252'>
+>>> fp2.encoding ➎
+'cp1252'
+>>> fp2.read()
+'cafÃ©' ➏
+>>> fp3 = open('cafe.txt', encoding='utf_8') ➐
+>>> fp3
+<_io.TextIOWrapper name='cafe.txt' mode='r' encoding='utf_8'>
+>>> fp3.read()
+'café' ➑
+>>> fp4 = open('cafe.txt', 'rb') ➒
+>>> fp4
+<_io.BufferedReader name='cafe.txt'> ➓
+>>> fp4.read() ⓫
+b'caf\xc3\xa9'
+除非想判断编码，否则不要在二进制模式中打开文本文件；
+即便如此，也应该使用 Charde
+关于编码默认值的最佳建议是:自己设定编码类型
 
+Unicode字符串规范化
+>>> from unicodedata import normalize
+>>> s1 = 'café' # 把"e"和重音符组合在一起
+>>> s2 = 'cafe\u0301' # 分解成"e"和重音符
+>>> len(s1), len(s2)
+(4, 5)
+>>> len(normalize('NFC', s1)), len(normalize('NFC', s2)) #NFC格式把字符和声标组合在一起
+(4, 4)
+>>> len(normalize('NFD', s1)), len(normalize('NFD', s2)) #NFD格式把字符和声标分开，所以有5个长度
+(5, 5)
+>>> normalize('NFC', s1) == normalize('NFC', s2)
+True
+>>> normalize('NFD', s1) == normalize('NFD', s2)
+True
+
+>>> from unicodedata import normalize, name
+>>> half = '½'
+>>> normalize('NFKC', half)
+'1⁄2'
+>>> four_squared = '4²'
+>>> normalize('NFKC', four_squared) #兼容模式
+'42'
+>>> micro = 'μ'
+>>> micro_kc = normalize('NFKC', micro)
+>>> micro, micro_kc
+('μ', 'μ')
+>>> ord(micro), ord(micro_kc)
+(181, 956)
+>>> name(micro), name(micro_kc)
+('MICRO SIGN', 'GREEK SMALL LETTER MU')
+使用 '1/2' 替代 '½' 可以接受，微符号也确实是小写的希腊字母
+'μ'，但是把 '4²' 转换成 '42' 就改变原意了。某些应用程序可以把
+'4²' 保存为 '4<sup>2</sup>'，但是 normalize 函数对格式一无所
+知。因此，NFKC 或 NFKD 可能会损失或曲解信息，但是可以为搜索和
+索引提供便利的中间表述：用户搜索 '1 / 2 inch' 时，如果还能找到
+包含 '½ inch' 的文档，那么用户会感到满意。
+使用 NFKC 和 NFKD 规范化形式时要小心，而且只能在特
+殊情况中使用，例如搜索和索引，而不能用于持久存储，因为这两
+种转换会导致数据损失。
