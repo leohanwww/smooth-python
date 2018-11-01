8802e3d189ee05d7fc00955026e62f4087fc885c
leohanwww <pumpkinlittlehan@hotmail.com>
2018-11-01 17:27:52 +0800

    开始抽象类

---
+++ 101 异常的结构.py
@@ -0,0 +1,22 @@
+异常类的部分层次结构
+
+BaseException
+├── SystemExit
+├── KeyboardInterrupt
+├── GeneratorExit
+└── Exception
+├── StopIteration
+├── ArithmeticError
+│ ├── FloatingPointError
+│ ├── OverflowError
+│ └── ZeroDivisionError
+├── AssertionError
+├── AttributeError
+├── BufferError
+├── EOFError
+├── ImportError
+├── LookupError 
+│ ├── IndexError 
+│ └── KeyError 
+├── MemoryError
+... etc.
--- 11 接口和抽象基类.py
+++ 11 接口和抽象基类.py
@@ -53,3 +53,122 @@ random.shuffle 函数不关心参数的类型，只要那个对象实现了部
 分可变序列协议即可。即便对象一开始没有所需的方法也没关系，后来
 再提供也行。
 
+鸭子类型
+x和y两个对象都有draw方法，x.draw()和y.draw()
+白鹅类型
+只要 cls 是抽象基类，即 cls 的元类是
+abc.ABCMeta，就可以使用 isinstance(obj, cls)
+
+Python 的抽象基类还有一个重要的实用优势：可以使用 register
+类方法在终端用户的代码中把某个类“声明”为一个抽象基类的“虚
+拟”子类（为此，被注册的类必须满足抽象基类对方法名称和签名
+的要求，最重要的是要满足底层语义契约；但是，开发那个类时不
+用了解抽象基类，更不用继承抽象基类）
+
+抽象基类的本质就是几个特殊方法
+>>> class Struggle:
+... def __len__(self): return 23
+...
+>>> from collections import abc
+>>> isinstance(Struggle(), abc.Sized)
+True
+无需注册，abc.Sized 也能把 Struggle 识别为自己的
+子类，只要实现了特殊方法 __len__ 即可（要使用正确的句法和
+语义实现，前者要求没有参数，后者要求返回一个非负整数，指明
+对象的长度；如果不使用规定的句法和语义实现特殊方法，如
+__len__，会导致非常严重的问题）。
+检查是不是序列
+isinstance(the_arg, collections.abc.Sequence)
+
+使用鸭子类型处理单个字符串或由字符串组成的可迭代对象
+try: 
+	field_name = field_name.replace(',' ' ').split() #把对象直接当成字符对象不用事先测试其类型
+except AttributeError： #假如不是字符串就没有replace方法
+	pass
+field_name = tuple(field_name)
+
+从抽象类继承
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck2(collections.MutableSequence): #字类
+	ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+	suits = 'spades diamonds clubs hearts'.split()
+	
+	def __init__(self):
+		self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+		
+	def __len__(self):
+		return len(self._cards)
+		
+	def __getitem__(self, position):
+		return self._cards[position]
+		
+	def __setitem__(self, position, value): #支持洗牌
+		self._cards[position] = value
+		
+	def __delitem__(self, position):
+		del self._cards[position]
+	# 但是继承 MutableSequence 的类必须实现 __delitem__ 方法，这是 MutableSequence 类的一个抽象方法。
+	def insert(self, position, value):
+		self._cards.insert(position, value)
+	# 此外，还要实现 insert 方法，这是 MutableSequence 类的第三个抽象方法。
+
+colletions.abc 模块中定义了16个抽象基类
+Iterable、Container 和 Sized
+　　各个集合应该继承这三个抽象基类，或者至少实现兼容的协
+议。Iterable 通过 __iter__ 方法支持迭代，Container 通过
+__contains__ 方法支持 in 运算符，Sized 通过 __len__ 方法支持
+len() 函数。
+Sequence、Mapping 和 Set是不可变集合类型
+Mappingview：映射方法 .items()、.keys() 和 .values() 返回
+的对象分别是 ItemsView、KeysView 和 ValuesView 的实例。
+
+Number
+Complex
+Real
+Rational
+Integral
+如果想检查一个数是不是整数，可以使用 isinstance(x, numbers.Integral) 这样代码就能接受 int、bool（int 的子类）
+如果一个值可能是浮点数类型，可以使用 isinstance(x,numbers.Real) 检查。这样代码就能接受bool、int、float、fractions.Fraction
+
+虚拟字类
+from random import randrange
+from tombola import Tombola
+@Tombola.register # ➊注册为Tombola这个抽象类的虚拟字类
+class TomboList(list): # ➋
+
+	def pick(self):
+		if self: # ➌
+			position = randrange(len(self))
+			return self.pop(position) # ➍
+		else:
+			raise LookupError('pop from empty TomboList')
+
+	load = list.extend
+	
+	def loaded(self):
+		return bool(self) # ➏
+		
+	def inspect(self):
+		return tuple(sorted(self))
+注册为虚拟子类的类需要实现虚拟父类的所有方法
+>>> from tombola import Tombola
+>>> from tombolist import TomboList
+>>> issubclass(TomboList, Tombola)
+True
+>>> t = TomboList(range(100))
+>>> isinstance(t, Tombola)
+True
+
+类的继承关系在一个特殊的类属性中指定—— __mro__，即方法
+解析顺序（Method Resolution Order）。这个属性的作用很简单，按顺序
+列出类及其超类，Python 会按照这个顺序搜索方法。 查看
+TomboList 类的 __mro__ 属性，你会发现它只列出了“真实的”超类，
+即 list 和 object：
+>>> TomboList.__mro__
+(<class 'tombolist.TomboList'>, <class 'list'>, <class 'object'>)
+Tombolist.__mro__ 中没有 Tombola，因此 Tombolist 没有从
+Tombola 中继承任何方法。
+
