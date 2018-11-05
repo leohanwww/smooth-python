7982f9722ad43c05ad9e3a164d59b722f5b63e5e
leohanwww <pumpkinlittlehan@hotmail.com>
2018-11-05 10:15:06 +0800

    完成

--- 12 继承.py
+++ 12 继承.py
@@ -2,8 +2,8 @@
 
 内置类型会忽略字类覆盖的方法
 class DoppleDice(dict):
-	def __set__item(self, key, value):
-		super.__set__item__(key, [value] * 2)
+	def __setitem__(self, key, value):
+		super.__setitem__(key, [value] * 2)
 		
 >>> dd = DoppleDice(one=1)
 >>> dd
@@ -66,4 +66,68 @@ class DoppleDice(dict):
 Python 编写的类，如 UserDict 或 MutableMapping，就不会受此影
 响。
 
+多重继承的顺序
 
+class A:
+	def ping(self):
+		print('ping', self)
+
+class B(A):
+	def pong(self):
+		print('pong:', self)
+		
+class C(A):
+	def pong(self):
+		print('PONG:', self)
+		
+class D(B, C):
+	def ping(self):
+		super().ping()
+		print('post-ping:', self)
+	def pingpong(self):
+		self.ping() # 第一个调用是 self.ping()，运行的是 D 类的 ping 方法，输出这一行和下一行。
+		super().ping() #按照__mro__顺序D-B-A，最后调用A的ping方法
+		self.pong() #找到B的pong方法
+		super().pong() #B的pong
+		C.pong(self) #C的pong
+
+>>> from diamond import D
+>>> d = D()
+>>> d.pingpong()
+ping: <diamond.D object at 0x10bf235c0> # ➊
+post-ping: <diamond.D object at 0x10bf235c0>
+ping: <diamond.D object at 0x10bf235c0> # ➋
+pong: <diamond.D object at 0x10bf235c0> # ➌
+pong: <diamond.D object at 0x10bf235c0> # ➍
+PONG: <diamond.D object at 0x10bf235c0> # ➎
+
+在 D 的实例上调用 d.pong() 方法的话，运行的是哪个 pong 方法呢？
+因为B和C都有pong方法
+>>> from diamond import *
+>>> d = D()
+>>> d.pong() # ➊ 直接调用 d.pong() 运行的是 B 类中的版本。
+pong: <diamond.D object at 0x10066c278>
+>>> C.pong(d) # ➋ 超类中的方法都可以直接调用，此时要把实例作为显式参数传入。
+PONG: <diamond.D object at 0x10066c278>
+此时查看D类的继承顺序
+>>> D.__mro__
+(<class 'diamond.D'>, <class 'diamond.B'>, <class 'diamond.C'>,
+<class 'diamond.A'>, <class 'object'>)
+
+有时可能需要绕过方法解析顺序，直接
+调用某个超类的方法——这样做有时更方便。例如，D.ping 方法可以
+这样写：
+def ping(self):
+	A.ping(self) # 而不是super().ping()
+	print('post-ping:', self)
+直接在类上调用实例方法时，必须显式传入 self 参数，因为这
+样访问的是未绑定方法（unbound method）。
+
+
+
+使用super()最为安全
+>>> from diamond import D
+>>> d = D()
+>>> d.ping() # ➊D类的ping方法做了两次调用
+ping: <diamond.D object at 0x10cc40630> # ➋第一个调用是super().ping(),super函数按照继承顺序把ping方法委托给了A类
+post-ping: <diamond.D object at 0x10cc40630> # ➌第二个调用是输出自身实例
